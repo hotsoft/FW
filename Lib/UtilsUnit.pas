20b62705cf095d3bbf53be543e23cf2cfae922e7
tiago <desenv6@labplus.com.br>
2014-05-19 16:16:40 -0300

    Ticket_ID: #14406 - Colocando função dentro de classe

--- Lib/UtilsUnit.pas
+++ Lib/UtilsUnit.pas
@@ -11,6 +11,10 @@ uses
 type
   varArrayOfcomps = array of TComponent;
 
+  THSHash = class
+    class function CalculaHash(conteudo: string): string;
+  end;
+  
 function isDigitOrControl(Key: char): boolean;
 function RemoveAcento(Str:String): String;
 procedure criarArquivoBackupIB(nomeArq: string);
@@ -62,7 +66,6 @@ function ConverteStrToDate(data: string): TDateTime;
 function ConverteStrToDate2(data: string): TDateTime;
 function ConverteStrToDate3(data: string): TDateTime;
 function GetIPAddress: string;
-function CalculaHash(conteudo: string): string;
 
 implementation
 
@@ -906,7 +909,7 @@ begin
   Result := Format('%d.%d.%d.%d', [BufferR[3], BufferR[2], BufferR[1], BufferR[0]]);
 end;
 
-function CalculaHash(conteudo: string): string;
+class function THSHash.CalculaHash(conteudo: string): string;
 var
   sum, i : Integer;
   HFrame : string;
