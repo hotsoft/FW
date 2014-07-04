103b53eef5574e2a51e1ba4731222b01cf54569e
Juliano <desenv4@labplus.com>
2014-07-04 08:26:35 -0300

    tirando limitação de tamanho dos componentes do email.

--- Lib/osErrorHandler.pas
+++ Lib/osErrorHandler.pas
@@ -325,7 +325,7 @@ end;
 
 function TosErrorHandlerForm.IsEmail(PField : TField) : boolean;
 begin
-  Result := ExecRegExpr('^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+\.[A-Za-z0-9-]{2,10}(\.[A-Za-z0-9-]{2,10})*$',
+  Result := ExecRegExpr('^[A-Za-z0-9._%+-]+@[A-Za-z0-9-]+\.[A-Za-z0-9-]{2,}(\.[A-Za-z0-9-]{2,})*$',
     PField.AsString);
 end;
 
