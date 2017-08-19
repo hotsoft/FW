c4bd525bc62fb241e19ac2cb225c522e120dca78
Wellington Torrejais da Silva <desenv2@labplus.com.br>
2017-08-19 19:02:34 -0300

    Ticket_id: #57666 - Correcao de Tipo
    Signed-off-by: Wellington Torrejais da Silva <desenv2@labplus.com.br>

--- Lib/UtilsUnit.pas
+++ Lib/UtilsUnit.pas
@@ -140,7 +140,7 @@ function ProcessExists(exeFileName: string; var FTaskName: string; var FPid: PDW
 function KillTask(const ExeFileName: string): Integer;
 function GetMD5FromString(const text: string): String;
 function GetPageAsstring(const url: string): String;
-function GetUrlWhitoutParams(const url: String): String;
+function GetUrlWithoutParams(const url: String): String;
 
 implementation
 
@@ -1776,7 +1776,7 @@ begin
   end;
 end;
 
-function GetUrlWhitoutParams(const url: String): String;
+function GetUrlWithoutParams(const url: String): String;
 var
   _uri: TIdURI;
 begin
