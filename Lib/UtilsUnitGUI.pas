ebdc6e7ba7b1e8e4997e8c94956a31df9fb76aa7
Danilo <desenv4@labplus.com.br>
2018-08-08 14:14:22 -0300

    Ticket_Id: #68847 - Método ConvertTextoToRTF aceita fontSize e FontName

--- Lib/UtilsUnitGUI.pas
+++ Lib/UtilsUnitGUI.pas
@@ -3,7 +3,7 @@ unit UtilsUnitGUI;
 interface
 
 uses
-  Vcl.Forms, Controls, ComCtrls, DBCtrls, wwdbdatetimepicker, Wwdbcomb, StdCtrls,  Buttons, Wwdbgrid,
+  Vcl.Forms, Vcl.Controls, ComCtrls, DBCtrls, wwdbdatetimepicker, Wwdbcomb, StdCtrls,  Buttons, Wwdbgrid,
   wwdbedit, acSysUtils, Printers, osComboSearch, System.Classes, DB, DBClient,  Winapi.PsApi, Winapi.Windows,
   Vcl.Graphics, ShellAPI, UMensagemAguarde, SysUtils, UtilsUnit, Variants, Winapi.Messages, Winapi.TlHelp32,
   Winsock;
@@ -31,7 +31,7 @@ procedure habilitaComponentes(comps: varArrayOfcomps);
 procedure desHabilitaComponentes(comps: array of TComponent);
 procedure ImprimirImpressoraTermica(const comando, impressora: String);
 function ConverteRTF(rtf: string): string;
-function ConverteTextoToRTF(Texto: string): string;
+function ConverteTextoToRTF(const Texto: string; FontSize: integer = 0; FontName: string= ''): string;
 function getCampoSemRTF(const vValor : Variant):String;
 function CriarMsgLogAlteracaoField(aField : TField):String; overload;
 function CriarMsgLogAlteracaoField(aField : TField; aFuncaoGetDescricao : TFuncaoParametroGetDesc):String; overload;
@@ -271,7 +271,7 @@ begin
   end;
 end;
 
-function ConverteTextoToRTF(Texto: string): string;
+function ConverteTextoToRTF(const Texto: string; FontSize: integer = 0; FontName: string= ''): string;
 var
   form: TForm;
   richEdit: TRichEdit;
@@ -284,6 +284,10 @@ begin
       form := TForm.Create(nil);
       richEdit := TRichEdit.Create(form);
       richEdit.Parent := form;
+      if FontSize > 0 then
+        richEdit.Font.Size := FontSize;
+      if not FontName.IsEmpty then
+        richEdit.Font.Name := FontName;
       richEdit.Text:= Texto;
       richEdit.PlainText := False;
       richEdit.Lines.SaveToStream(ss);
