efac05f8e7e4e6a039b7710073fe64c640ff5933
Danilo <desenv4@labplus.com.br>
2018-08-14 09:10:57 -0300

    Ticket_Id: #69392 - Tratamento de IOHandler para HTTPS

--- Lib/UtilsUnit.pas
+++ Lib/UtilsUnit.pas
@@ -1926,10 +1926,16 @@ function getJsonStringFromServer(const aURL: string; var aException: string): st
 var
   _http: TIdHTTP;
   _Response: TStringStream;
+  IOHandler: TIdSSLIOHandlerSocketOpenSSL;
 begin
   aException := EmptyStr;
   _Response := TStringStream.Create(EmptyStr, TEncoding.UTF8);
   _http := GetIdHttp;
+  if aURL.ToLower.Contains('https') then
+  begin
+    IOHandler := TIdSSLIOHandlerSocketOpenSSL.Create(_http);
+    _http.IOHandler := IOHandler;
+  end;
   try
     try
       _http.Get(aURL, _Response);
@@ -1943,6 +1949,8 @@ begin
       aException := UtilsUnit.HandleException(aURL);
     end;
   finally
+    if aURL.ToLower.Contains('https') then
+      FreeAndNil(IOHandler);
     FreeAndNil(_http);
     FreeAndNil(_Response);
   end;
