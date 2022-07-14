db90457192469ac45fcb4e9140abb159a5366188
Francisco <desenv7@labplus.com.br>
2022-07-14 09:43:34 -0300

    ticket_id: #105999 - Publicacao Digital em DLL, fazer com que a publicacao manual, exiba o texto do console se houver erro na publicacao

--- Lib/UtilsUnitGUI.pas
+++ Lib/UtilsUnitGUI.pas
@@ -53,6 +53,7 @@ function GetTaskHandle(const ATaskName : string; var FTaskName: String; var FPid
   var FProcessa: Boolean; var FHWND: HWND; var iListOfProcess: Integer) : HWND;
 procedure ExecuteAndWait(const aCommando: string);
 function Execute(const aCommando: string; const ShowWindow: boolean; var aProcessInformation: TProcessInformation): boolean;
+function GetDosOutput(CommandLine: string): string;
 procedure WaitProcess(const aProcessInformation: TProcessInformation; aCheckIsAlive: boolean; aThreadId: TThreadID; const aPort: integer);
 procedure CloseProcess(const aProcessInformation: TProcessInformation);
 function LocalIp: string;
@@ -667,6 +668,60 @@ begin
   begin
     Result := False;
     RaiseLastOSError;
+  end;
+end;
+
+function GetDosOutput(CommandLine: string): string;
+var
+  SA: TSecurityAttributes;
+  SI: TStartupInfo;
+  PI: TProcessInformation;
+  StdOutPipeRead, StdOutPipeWrite: THandle;
+  WasOK: Boolean;
+  Buffer: array[0..255] of AnsiChar;
+  BytesRead: Cardinal;
+  WorkDir: string;
+  Handle: Boolean;
+begin
+  Result := '';
+  with SA do begin
+    nLength := SizeOf(SA);
+    bInheritHandle := True;
+    lpSecurityDescriptor := nil;
+  end;
+  CreatePipe(StdOutPipeRead, StdOutPipeWrite, @SA, 0);
+  try
+    with SI do
+    begin
+      FillChar(SI, SizeOf(SI), 0);
+      cb := SizeOf(SI);
+      dwFlags := STARTF_USESHOWWINDOW or STARTF_USESTDHANDLES;
+      wShowWindow := SW_HIDE;
+      hStdInput := GetStdHandle(STD_INPUT_HANDLE); // don't redirect stdin
+      hStdOutput := StdOutPipeWrite;
+      hStdError := StdOutPipeWrite;
+    end;
+    Handle := CreateProcess(nil, PChar(CommandLine),
+                            nil, nil, True, 0, nil,
+                            nil, SI, PI);
+    CloseHandle(StdOutPipeWrite);
+    if Handle then
+      try
+        repeat
+          WasOK := ReadFile(StdOutPipeRead, Buffer, 255, BytesRead, nil);
+          if BytesRead > 0 then
+          begin
+            Buffer[BytesRead] := #0;
+            Result := Result + Buffer;
+          end;
+        until not WasOK or (BytesRead = 0);
+        WaitForSingleObject(PI.hProcess, INFINITE);
+      finally
+        CloseHandle(PI.hThread);
+        CloseHandle(PI.hProcess);
+      end;
+  finally
+    CloseHandle(StdOutPipeRead);
   end;
 end;
 
